fc9509f79bacc005a7ad8757d04bfcca5f30637d
gabriel <gabriel.dadamosrossetto@gmail.com>
2021-01-24 01:20:04 -0300

    master | Config do projeto + testando call api destaques

---
+++ src/pages/home/home.js
@@ -0,0 +1,54 @@
+import "./home.styles.scss";
+import React, { useEffect, useState } from "react";
+import { getConfig, getMovies } from "../../services/movies.service.ts";
+
+const Home = () => {
+  const [featured, setFeatured] = useState([]);
+  const [config, setConfig] = useState({});
+
+  async function getFeaturedMovies() {
+    let movies = await getMovies();
+    setFeatured(movies?.results);
+  }
+
+  async function getMovieDbConfig() {
+    let configuration = await getConfig();
+    setConfig(configuration?.images);
+  }
+
+  useEffect(() => {
+    getMovieDbConfig();
+  }, []);
+
+  useEffect(() => {
+    getFeaturedMovies();
+  }, [config]);
+
+  return (
+    <main className="bg-gray-700 p-6">
+      <h1 className="text-green-200 font-bold text-6xl text-center">
+        Featured Movies
+      </h1>
+      <div className="featured-movies">
+        {featured.map((movie, index) => {
+          return (
+            <article
+              className="mb-8 shadow-xl p-4 hover:bg-green-300"
+              key={movie?.id}
+            >
+              <img
+                className={"w-1/4"}
+                src={`${config.base_url}${config.poster_sizes[3]}${movie.poster_path}`}
+                alt=""
+              />
+            </article>
+          );
+        })}
+        <div className="invisible"></div>
+        <div className="invisible"></div>
+      </div>
+    </main>
+  );
+};
+
+export default Home;
---
+++ src/services/movies.service.ts.js
@@ -0,0 +1,13 @@
+export async function getMovies(){
+
+    let movies = await fetch('https://api.themoviedb.org/3/trending/movie/week?api_key=476a7e901b8459552d81da8f4a322ae7');
+    let moviesJson = await movies.json();
+    return moviesJson;
+
+}
+
+export async function getConfig(){
+    let config = await fetch('https://api.themoviedb.org/3/configuration?api_key=476a7e901b8459552d81da8f4a322ae7');
+    let configJson = await config.json();
+    return configJson;
+}
